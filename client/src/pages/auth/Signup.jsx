ff077164705dee2b9cf85135ae36c87cda7a768e
Zomer <zomerzxc.19@gmail.com>
2021-09-23 02:44:49 +0800

    Add signup form

--- client/src/pages/auth/Signup.jsx
+++ client/src/pages/auth/Signup.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState();
+
+  const handleSubmit = () => {};
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='email'
+        className='mb-2 form-control'
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        autoFocus
+      />
+      <button type='submit' className=' btn btn-outline-primary'>
+        Sign up
+      </button>
+    </form>
+  );
+
+  return (
+    <div className='container p-5'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          <h4>Sign up</h4>
+          {registerForm()}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
