72a6934cb95a8b4dda232da54860316701728592
Zomer <zomerzxc.19@gmail.com>
2021-09-23 06:05:14 +0800

    Add logout

--- client/src/components/nav/Header.jsx
+++ client/src/components/nav/Header.jsx
@@ -5,16 +5,32 @@ import {
   SettingOutlined,
   UserOutlined,
   UserAddOutlined,
+  LogoutOutlined,
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import firebase from 'firebase';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState('home');
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const handleClick = e => {
     setCurrent(e.key);
+  };
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: 'LOGOUT',
+      payload: null,
+    });
+
+    history.push('/login');
   };
 
   return (
@@ -31,13 +47,16 @@ const Header = () => {
       <SubMenu key='SubMenu' icon={<SettingOutlined />} title='Username'>
         <Item key='setting:1'>Option 1</Item>
         <Item key='setting:2'>Option 2</Item>
+        <Item icon={<LogoutOutlined />} key='setting:2' onClick={handleLogout}>
+          Logout
+        </Item>
       </SubMenu>
 
-      <Item key='login' icon={<UserOutlined />} className='float-end'>
-        <Link to='/login'>Log in</Link>
-      </Item>
       <Item key='signup' icon={<UserAddOutlined />} className='float-end'>
         <Link to='/signup'>Sign up</Link>
+      </Item>
+      <Item key='login' icon={<UserOutlined />} className='float-end'>
+        <Link to='/login'>Log in</Link>
       </Item>
     </Menu>
   );
