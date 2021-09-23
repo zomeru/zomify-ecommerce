e7da1d27942035e1410f93fabded19ecff629738
Zomer <zomerzxc.19@gmail.com>
2021-09-23 08:01:15 +0800

    Add login with email

--- client/src/components/nav/Header.jsx
+++ client/src/components/nav/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
       <SubMenu key='SubMenu' icon={<SettingOutlined />} title='Username'>
         <Item key='setting:1'>Option 1</Item>
         <Item key='setting:2'>Option 2</Item>
-        <Item icon={<LogoutOutlined />} onClick={handleLogout}>
+        <Item icon={<LogoutOutlined />} key='logout' onClick={handleLogout}>
           Logout
         </Item>
       </SubMenu>
--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -3,14 +3,36 @@ import { auth } from '../../firebase';
 import { toast } from 'react-toastify';
 import { Button } from 'antd';
 import { MailOutlined } from '@ant-design/icons';
+import { useDispatch } from 'react-redux';
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({ history }) => {
+  const [email, setEmail] = useState('zomerzxc.19@gmail.com');
+  const [password, setPassword] = useState('123456');
+  const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch();
 
   const handleSubmit = async e => {
     e.preventDefault();
-    console.table(email, password);
+    setLoading(true);
+    try {
+      const result = await auth.signInWithEmailAndPassword(email, password);
+      // console.log(result);
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+      dispatch({
+        type: 'LOGGED_IN_USER',
+        payload: {
+          email: user.email,
+          token: idTokenResult.token,
+        },
+      });
+      history.push('/');
+    } catch (error) {
+      console.log(error);
+      console.log(error.message);
+      setLoading(false);
+    }
   };
 
   const loginForm = () => (
