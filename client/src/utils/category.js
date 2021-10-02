468a64feee6ea8962597368b4fcfd2a336b1f8da
Zomer <zomerzxc.19@gmail.com>
2021-10-02 15:38:20 +0800

    Create category CRUD requests

---
+++ client/src/utils/category.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export const getCategories = async () =>
+  await axios.get(`${import.meta.env.VITE_API_ENDPOINT}/categories`);
+
+export const getCategory = async slug =>
+  await axios.get(`${import.meta.env.VITE_API_ENDPOINT}/category/${slug}`);
+
+export const removeCategory = async (slug, authtoken) =>
+  await axios.delete(`${import.meta.env.VITE_API_ENDPOINT}/category/${slug}`, {
+    headers: {
+      authtoken,
+    },
+  });
+
+export const updateCategory = async (slug, category, authtoken) =>
+  await axios.put(`${import.meta.env.VITE_API_ENDPOINT}/category/${slug}`, {
+    headers: {
+      authtoken,
+    },
+  });
+
+export const createCategory = async (category, authtoken) =>
+  await axios.post(`${import.meta.env.VITE_API_ENDPOINT}/category`, category, {
+    headers: {
+      authtoken,
+    },
+  });
