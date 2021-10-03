4fe84093ecda181e30e16396a6aaf7e6909cc965
Zomer <zomerzxc.19@gmail.com>
2021-10-03 12:40:06 +0800

    ✏ Hide firebase key

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -1,9 +1,15 @@
 var admin = require('firebase-admin');
 
-var serviceAccount = require('../config/firebaseServiceAccountKey.json');
+var serviceAccount = require('../config/firebaseServiceAccountKey.js');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(
+    JSON.parse(
+      JSON.stringify(serviceAccount)
+        .replace(/\\/g, 'ZOM')
+        .replaceAll('ZOMZOM', '\\')
+    )
+  ),
 });
 
 module.exports = admin;
